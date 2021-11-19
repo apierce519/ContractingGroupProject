e02155b098c23f94cd904cfc8f036742593d2fbb
apierce519 <a.pierce519@gmail.com>
2021-11-18 20:29:41 -0600

    Sql statement changes

--- src/main/java/login/beans/Operations.java
+++ src/main/java/login/beans/Operations.java
@@ -5,14 +5,18 @@
  */
 package login.beans;
 
+import java.sql.PreparedStatement;
+
 /**
  * @author Andrew Pierce - ajpierce1
  */
 public class Operations {
 
 	public static boolean isLogin(String username, String password, String userType) {
-		String mySqlQuery = "SELECT id, userType FROM user WHERE userName = '" + username + "' and password = '" + password + "' AND userType = '" + "'" ;
+		String mySqlQuery = "SELECT id, user_type FROM user WHERE userName = '" + username + "' AND password = '" + password + "' AND user_type = '" + userType + "'" ;
 		return false;
+		
+		PreparedStatement ps = myCon.prepareStatment(mySqlQuery);
 	}
 	
 }
